f640ebdb3032251482fb1e6a75acc5e2e13b54fe
Max Kohler <maxkohler90@web.de>
2016-07-26 12:40:09 +0200

    Add files via upload

---
+++ Application/SerialConsoleApplication/SerialConsoleApplication/commands/simplecommands/AddRepeatingAccuracyMeasurementSeriesToMeasurementSeriesCollectionCommand.cs
@@ -0,0 +1,31 @@
+using commands.reactivecommands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace commands
+{
+    namespace simplecommands
+    {
+        public class AddRepeatingAccuracyMeasurementSeriesToMeasurementSeriesCollectionCommand : SimpleCommand
+        {
+
+            public MeasurementSeriesCollection msc;
+            public String name;
+
+
+            public AddRepeatingAccuracyMeasurementSeriesToMeasurementSeriesCollectionCommand(String name, MeasurementSeriesCollection msc)
+            {
+              this.msc = msc;
+              this.name = name;
+            }
+            public void execute()
+            {
+                this.msc.addMeasurementSeries(new RepeatingAccuracyMeasurementSeries(this.name));
+            }
+
+        }
+    }
+}
