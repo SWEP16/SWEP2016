01fa2189f26e26a7718c275e6cf6168d37ecbf80
Alex <Alex@WIN-062CHQ71NQ8>
2016-07-27 14:40:58 +0200

    AutoZero

---
+++ Application/SerialConsoleApplication/SerialConsoleApplication/commands/reactiveFullCommands/AutoZeroOffCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using usb;
+
+
+namespace commands
+{
+    namespace reactivecommands
+    {
+        public class AutoZeroOffCommand : ReactiveFullCommand
+        {
+            public AutoZeroOffCommand()
+            {
+
+            }
+
+            public override void executeOnPort1(System.IO.Ports.SerialPort port1)
+            {
+                String command = "W0\r";
+                byte[] commandByteArray = StringToHexParser.parse(command);
+                port1.Write(commandByteArray, 0, commandByteArray.Length);
+                Console.WriteLine("EXECUTE AUTO ZERO OFF COMMAND on Port 1");
+            }
+
+            public override void executeOnPort2(System.IO.Ports.SerialPort port2)
+            {
+                String command = "W0\r";
+                byte[] commandByteArray = StringToHexParser.parse(command);
+                port2.Write(commandByteArray, 0, commandByteArray.Length);
+                Console.WriteLine("EXECUTE AUTO ZERO OFF COMMAND on Port 2");
+            }
+
+            public override void react(char[] answerData1, char[] answerData2)
+            {
+                Console.WriteLine("REACT AutoZeroOffCommand");
+                Console.WriteLine(answerData1);
+                Console.WriteLine(answerData2);
+            }
+
+            public override bool isCorrectAnswerFormat(char[] answerData)
+            {
+                return true;
+            }
+        }
+    }
+}
