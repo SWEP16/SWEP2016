8bec9f19ac878d7a675f55c0be26c9b4cd0bd2e1
Alex <Alex@WIN-062CHQ71NQ8>
2016-07-27 10:56:02 +0200

    AddWayTimeMeasurementSeriesToMeasurementSeriesCollectionCommand

---
+++ Application/SerialConsoleApplication/SerialConsoleApplication/commands/simplecommands/AddWayTimeMeasurementSeriesToMeasurementSeriesCollectionCommand.cs
@@ -0,0 +1,31 @@
+using commands.reactivecommands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace commands
+{
+    namespace simplecommands
+    {
+        public class AddWayTimeMeasurementSeriesToMeasurementSeriesCollectionCommand : SimpleCommand
+        {
+
+            public MeasurementSeriesCollection msc;
+            public String name;
+
+
+            public AddWayTimeMeasurementSeriesToMeasurementSeriesCollectionCommand(String name, MeasurementSeriesCollection msc)
+            {
+              this.msc = msc;
+              this.name = name;
+            }
+            public void execute()
+            {
+                this.msc.addMeasurementSeries(new WayTimeMeasurementSeries(this.name));
+            }
+
+        }
+    }
+}
